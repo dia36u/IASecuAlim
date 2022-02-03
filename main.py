15b5b0a52c13d64d3b232b8fed0483e1e37e7a92
dia36u <dia.babacar1@gmail.com>
2022-02-03 18:49:11 +0100

    comment de l'import processusETL

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from database import create_database
 from app import create_app
-from modele import processus_ETL
+# from modele import processus_ETL
 
 database = create_database()
 app = create_app()
