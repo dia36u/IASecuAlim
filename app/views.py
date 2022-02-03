824a0caa52a3a6463ca44e6f86cbc97805756ab3
dia36u <dia.babacar1@gmail.com>
2022-02-03 18:34:13 +0100

    ajout des liste déroulante

--- app/views.py
+++ app/views.py
@@ -1,13 +1,11 @@
-from multiprocessing.connection import Connection
 from flask import Blueprint, render_template, request
-<<<<<<< HEAD
-from database import Classes
-=======
 from flask_login import login_required, current_user
 import random
 from .models import Estimation
 from . import db
->>>>>>> 23b15cc3a4bf46a88ab4ec38cff7494c0bef1e90
+import pandas as pd
+from modele.RandomForest import predict
+from database.Classes import Connection
 
 views = Blueprint('views', __name__)
 
@@ -17,10 +15,10 @@ def profil():
     # Affiche les données personnels du current-user
     return render_template("views/user_profil.html", user=current_user)
 
-@views.route('/etablissement')
-@login_required
-def etablissement():
-    return render_template("views/add_etablissement.html", user=current_user)
+# @views.route('/etablissement')
+# @login_required
+# def etablissement():
+#     return render_template("views/add_etablissement.html", user=current_user)
 
 @views.route('/historique')
 @login_required
@@ -31,23 +29,32 @@ def historique():
 @views.route('/etablissement',methods=['POST','GET'])
 @login_required
 def estimation():
-    activites_domaines = Classes.Connection.query_table('domaine_activite')
-    print(activites_domaines)
     # Affiche un formulaire de demande de prédiction
+    requete_id='SELECT * FROM domaine_activite'
+    db = Connection.connection_to_database()
+    activites = Connection.query_all(db, requete_id)
+    requete_id='SELECT * FROM type_activite'
+    types=Connection.query_all(db, requete_id)
+
     if request.method == 'POST':
-                libelle = request.form.get('libelle')
-                siret = request.form.get('siret')
-                libelle_activite = request.form.get('libelle_activite')
-                niveau_hygiene= ['Très satisfaisant','Satisfaisant','A améliorer','A corriger de manière urgente']
-                niveau_hygiene=(random.choice(niveau_hygiene))
-                new_estimation = Estimation(result=(libelle+' ('+siret+') '+': '+niveau_hygiene), user_id=current_user.id)
-                db.session.add(new_estimation)
-                db.session.commit()
-                return render_template("views/estimation.html",
-<<<<<<< HEAD
-                libelle=libelle,siret=siret,libelle_activite=libelle_activite,niveau_hygiene="très satisfaisant")
-    return render_template("views/add_etablissement.html", domaines=activites_domaines)
-=======
-                libelle=libelle,siret=siret,libelle_activite=libelle_activite,niveau_hygiene=niveau_hygiene, user=current_user)
-    return render_template("views/add_etablissement.html", user=current_user)
->>>>>>> 23b15cc3a4bf46a88ab4ec38cff7494c0bef1e90
+        libelle = request.form.get('libelle')
+        siret = request.form.get('siret')
+        libelle_activite = request.form.get('libelle_activite')
+        code_postal = request.form.get('code_postal')
+        type_activite = request.form.get('type_activite')
+        # niveau_hygiene= ['Très satisfaisant','Satisfaisant','A améliorer','A corriger de manière urgente']
+        # niveau_hygiene=(random.choice(niveau_hygiene))
+        # new_estimation = Estimation(result=(libelle+' ('+siret+') '+': '+niveau_hygiene), user_id=current_user.id)
+        new_estimation = pd.DataFrame({"id_activite": [libelle_activite], "id_type_activite": [type_activite], "code_postal":[code_postal]})
+        # db.session.add(new_estimation)
+        # db.session.commit()
+        # get prediction for new input
+        new_output = predict.cls.predict(new_estimation)
+        niveau_hygiene = Connection.query_all(db, 'SELECT * FROM niveau_hygiene')
+        for niveau in niveau_hygiene:
+            if new_output==niveau[0]:
+                niveau_textuel=niveau[1]
+        # summarize input and output
+        # print(new_input, new_output)
+        return render_template("views/estimation.html",libelle=libelle,siret=siret,libelle_activite=libelle_activite,niveau_hygiene=niveau_textuel, user=current_user)
+    return render_template("views/add_etablissement.html", user=current_user, activites=activites, types=types)
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from database import create_database
 from app import create_app
+from modele import processus_ETL
 
-# Création de la database Postgres et récup d'un objet connection
 database = create_database()
-
-# Création de l'app Flask
 app = create_app()
+# processus_ETL.launchETL()
 
 # Lancement de l'application
 if __name__ == "__main__":
     app.run()
 
-# Modele Regression Logistique
-#exec(open("modele/logisticRegression.py",'r').read())
